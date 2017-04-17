84c1517b7873028743a8840d53cd4a3881055c5a
Paulius Masiliunas <pmasiliunas@gmail.com>
2017-04-17 15:04:29 +0300

    change config path in tiles application

--- tiles.py
+++ tiles.py
@@ -2,4 +2,4 @@ from os import environ
 import TileStache
 
 # read config file or URL from environment, defaults to tiles.cfg
-application = TileStache.WSGITileServer('tiles.cfg')
+application = TileStache.WSGITileServer('/home/osm/tiles.cfg')
