f2c1c4efc3a6672d5225043a1b2866370bf80215
tomass <tomasstraupis@gmail.com>
2017-12-30 18:59:04 +0200

    Daugiau laiko informacijos loguose db atnaujinimo metu

--- db/update.sh
+++ db/update.sh
@@ -46,22 +46,46 @@ md5sum $DATAFILE > $DATAFILE.md5
 
 # render expired
 if [ -s dirty_tiles ]; then
-         
+    echo "Preparing tile lists " `date`
     grep -E "^(15|16|17|18)" dirty_tiles > delete_openmap_$DIRTY_FILE
-    grep -E -v "^(15|16|17|18)" dirty_tiles > generate_openmap_$DIRTY_FILE
+    grep -E "^(14)" dirty_tiles > generate_openmap_14_$DIRTY_FILE
+    grep -E "^(13)" dirty_tiles > generate_openmap_13_$DIRTY_FILE
+    grep -E "^(12)" dirty_tiles > generate_openmap_12_$DIRTY_FILE
+    grep -E "^(11)" dirty_tiles > generate_openmap_11_$DIRTY_FILE
+    grep -E "^(10)" dirty_tiles > generate_openmap_10_$DIRTY_FILE
+    grep -E "^(9)"  dirty_tiles > generate_openmap_9_$DIRTY_FILE
+    grep -E "^(8)"  dirty_tiles > generate_openmap_8_$DIRTY_FILE
 
     grep -E "^(10|11|12|13|14)" dirty_tiles > generate_bicycle_$DIRTY_FILE
     grep -E "^(10|11|12|13|14)" dirty_tiles > generate_craftbeer_$DIRTY_FILE
 
-    echo "OpenMap.lt render expired"
+    echo "OpenMap.lt delete expired " `date`
     tilestache-clean -c $TILESTACHE_CONFIG_FILE -l all -e pbf --tile-list delete_openmap_$DIRTY_FILE
-    tilestache-seed -c $TILESTACHE_CONFIG_FILE -x -l all -e pbf --tile-list generate_openmap_$DIRTY_FILE
+    echo "OpenMap.lt generate expired 14 " `date`
+    tilestache-seed -c $TILESTACHE_CONFIG_FILE -x -l all -e pbf --tile-list generate_openmap_14_$DIRTY_FILE
+    echo "OpenMap.lt generate expired 13 " `date`
+    tilestache-seed -c $TILESTACHE_CONFIG_FILE -x -l all -e pbf --tile-list generate_openmap_13_$DIRTY_FILE
+    echo "OpenMap.lt generate expired 12 " `date`
+    tilestache-seed -c $TILESTACHE_CONFIG_FILE -x -l all -e pbf --tile-list generate_openmap_12_$DIRTY_FILE
+    echo "OpenMap.lt generate expired 11 " `date`
+    tilestache-seed -c $TILESTACHE_CONFIG_FILE -x -l all -e pbf --tile-list generate_openmap_11_$DIRTY_FILE
+    echo "OpenMap.lt generate expired 10 " `date`
+    tilestache-seed -c $TILESTACHE_CONFIG_FILE -x -l all -e pbf --tile-list generate_openmap_10_$DIRTY_FILE
+    echo "OpenMap.lt generate expired 9 " `date`
+    tilestache-seed -c $TILESTACHE_CONFIG_FILE -x -l all -e pbf --tile-list generate_openmap_9_$DIRTY_FILE
+    echo "OpenMap.lt generate expired 8 " `date`
+    tilestache-seed -c $TILESTACHE_CONFIG_FILE -x -l all -e pbf --tile-list generate_openmap_8_$DIRTY_FILE
 
+    echo "Bicycle delete expired " `date`
     tilestache-clean -c $TILESTACHE_CONFIG_FILE -l bicycle -e pbf --tile-list delete_openmap_$DIRTY_FILE
+    echo "Bicycle generate expired " `date`
     tilestache-seed -c $TILESTACHE_CONFIG_FILE -x -l bicycle -e pbf --tile-list generate_bicycle_$DIRTY_FILE
 
+    echo "Craftbeer delete expired " `date`
     tilestache-clean -c $TILESTACHE_CONFIG_FILE -l craftbeer -e pbf --tile-list delete_openmap_$DIRTY_FILE
+    echo "Craftbeer generate expired " `date`
     tilestache-seed -c $TILESTACHE_CONFIG_FILE -x -l craftbeer -e pbf --tile-list generate_craftbeer_$DIRTY_FILE
+    echo "Done " `date`
 
     rm delete_openmap_$DIRTY_FILE generate_openmap_$DIRTY_FILE
     rm generate_bicycle_$DIRTY_FILE
