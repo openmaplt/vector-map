47af18150d75695e50c4d905d557c05314705cd9
tomass <tomasstraupis@gmail.com>
2017-12-31 00:26:19 +0200

    POI lentelė pakeista į materialized view

--- db/table_poi.sql
+++ db/table_poi.sql
@@ -1,37 +1,32 @@
-drop table poi_test;
-
-create table poi_test (
-  name text
- ,amenity text
- ,man_made text
- ,"tower:type" text
- ,tourism text
- ,"attraction:type" text
- ,access text
- ,historic text
- ,site_type text
- ,shop text
- ,information text
- ,office text
- ,official_name text
- ,alt_name text
- ,opening_hours text
- ,website text
- ,image text
- ,"ref:lt:kpd" text
- ,height text
- ,wikipedia text
- ,fee text
- ,email text
- ,phone text
- ,"addr:city" text
- ,"addr:street" text
- ,"addr:housenumber" text
-);
-
-select addgeometrycolumn('poi_test', 'way', 3857, 'POINT', 2);
-
-insert into poi_test
+create materialized view poi (
+  name
+ ,amenity
+ ,man_made
+ ,"tower:type"
+ ,tourism
+ ,"attraction:type"
+ ,access
+ ,historic
+ ,site_type
+ ,shop
+ ,information
+ ,office
+ ,official_name
+ ,alt_name
+ ,opening_hours
+ ,website
+ ,image
+ ,"ref:lt:kpd"
+ ,height
+ ,wikipedia
+ ,fee
+ ,email
+ ,phone
+ ,"addr:city"
+ ,"addr:street"
+ ,"addr:housenumber"
+ ,way
+) as
   select name
         ,amenity
         ,man_made
@@ -101,7 +96,3 @@ insert into poi_test
       or tourism is not null
       or historic is not null
       or office is not null;
-
-select count(1) from poi_test;
-
-vacuum analyze;
