94a44be1ce509f6801af2f28b527245a070dbc1e
tomass <tomasstraupis@gmail.com>
2018-05-14 15:56:28 +0300

    Topografinio žemėlapio papildymai

---
+++ db/table_poi_topo.sql
@@ -0,0 +1,97 @@
+drop materialized view if exists poi_topo;
+create materialized view poi_topo (
+  id
+ ,__type__
+ ,name
+ ,amenity
+ ,man_made
+ ,"tower:type"
+ ,tourism
+ ,"attraction:type"
+ ,access
+ ,historic
+ ,site_type
+ ,shop
+ ,information
+ ,office
+ ,official_name
+ ,alt_name
+ ,opening_hours
+ ,website
+ ,image
+ ,"ref:lt:kpd"
+ ,religion
+ ,aeroway
+ ,power
+ ,landuse
+ ,building
+ ,way
+) as
+  select
+        osm_id
+        ,'n' -- always node
+        ,name
+        ,amenity
+        ,man_made
+        ,"tower:type"
+        ,tourism
+        ,"attraction:type"
+        ,access
+        ,historic
+        ,site_type
+        ,shop
+        ,information
+        ,office
+        ,official_name
+        ,alt_name
+        ,opening_hours
+        ,website
+        ,image
+        ,"ref:lt:kpd"
+        ,religion
+        ,aeroway
+        ,power
+        ,landuse
+        ,building
+        ,way
+    from planet_osm_point
+   where aeroway in ('aerodrome', 'helipad')
+      or amenity = 'place_of_worship'
+      or man_made in ('chimnei', 'windmill', 'watermill', 'tower')
+      or amenity = 'fuel'
+      or power = 'substation'
+  union
+  select
+        ABS(osm_id)
+        ,CASE WHEN osm_id < 0 THEN 'r' ELSE 'w' END  -- r relation, w way
+        ,name
+        ,amenity
+        ,man_made
+        ,"tower:type"
+        ,tourism
+        ,null --"attraction:type"
+        ,access
+        ,historic
+        ,site_type
+        ,shop
+        ,information
+        ,office
+        ,official_name
+        ,alt_name
+        ,opening_hours
+        ,website
+        ,image
+        ,"ref:lt:kpd"
+        ,religion
+        ,aeroway
+        ,power
+        ,landuse
+        ,building
+        ,st_centroid(way)
+    from planet_osm_polygon
+   where aeroway in ('aerodrome', 'helipad')
+      or amenity = 'place_of_worship'
+      or man_made in ('chimnei', 'windmill', 'watermill', 'tower')
+      or amenity = 'fuel'
+      or landuse = 'quary'
+      or power = 'substation';
