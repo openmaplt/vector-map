74d043be4acc6deec4783ad34c2383347bc93f66
Paulius Masilūnas <pmasiliunas@gmail.com>
2018-08-02 18:04:17 +0300

    Delete search update script

--- db/update_search.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-echo "Start update Nominatim"    
-
-sudo -u postgres /home/nominatim/build/utils/update.php --import-diff ./change.osc --index                                                                
-
-echo "Finish update Nominatim"
