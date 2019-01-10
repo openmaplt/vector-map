e53605a3a2956a43fcc088e1e2dac09b60e83a29
tomass <tomasstraupis@gmail.com>
2019-01-10 22:43:20 +0200

    Konservatyvesnis miškų ir vandens generalizavimas

--- db/gen_water.sql
+++ db/gen_water.sql
@@ -19,20 +19,20 @@ insert into gen_water
   select nextval('gen_water_seq') AS id,
          0,
          150,
-         ST_CollectionExtract(unnest(ST_ClusterWithin(way, 150)), 3)::geometry(MultiPolygon, 3857)
+         ST_CollectionExtract(unnest(ST_ClusterWithin(way, 100)), 3)::geometry(MultiPolygon, 3857)
     from gen_water
    where res = 10;
 
-delete from gen_water where st_area(st_buffer(way, -75)) < 150 and res = 150;
+delete from gen_water where st_area(st_buffer(way, -50)) < 150 and res = 150;
 
-update gen_water set way = st_multi(st_simplifypreservetopology(st_buffer(st_buffer(way, 75, 'quad_segs=1'), -75, 'quad_segs=1'), 150)) where res = 150;
+update gen_water set way = st_multi(st_simplifypreservetopology(st_buffer(st_buffer(st_buffer(way, 50, 'quad_segs=1'), -100, 'quad_segs=1'), 50, 'quad_segs=1'), 100)) where res = 150;
 update gen_water set way_area = st_area(way) where res = 150;
 
 insert into gen_water
   select nextval('gen_water_seq') AS id,
          0,
          600,
-         ST_CollectionExtract(unnest(ST_ClusterWithin(way, 600)), 3)::geometry(MultiPolygon, 3857)
+         ST_CollectionExtract(unnest(ST_ClusterWithin(way, 300)), 3)::geometry(MultiPolygon, 3857)
     from gen_water
    where res = 150;
 
