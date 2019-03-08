62b2ad29f9971bf0bed605c4390ad94de114d590
tomass <tomasstraupis@gmail.com>
2019-03-08 20:01:17 +0200

    Vandens telkinių etikečių duomenys kaladėlėse

---
+++ data/labels/center.sql
@@ -0,0 +1,9 @@
+select id as gid
+      ,st_asbinary(way) as geom
+      ,name
+      ,size as font_size
+      ,spacing as letter_spacing
+  from car_centerline
+ where way && !bbox!
+   and ((zoom = !zoom!) or
+        (!zoom! = 18 and zoom = 17))
