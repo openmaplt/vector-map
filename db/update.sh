34efbfdced23aceea6490922746fc6814e78ac82
Ramūnas Dronga <github@ramuno.lt>
2019-04-07 18:24:19 +0300

    supaprastintas atnaujinimo skriptas, greitesnins kaladeliu valymas ir pergeneruojamas 15 zoom lygis

--- db/update.sh
+++ db/update.sh
@@ -2,6 +2,7 @@
 DATAFILE=${DATAFILE:=data.pbf}
 DIRTY_FILE=dirty_tiles_`date +%s`
 TEGOLA_CONFIG_FILE=$(readlink -f ./../config.toml)
+TEGOLA_SEED="../tegola cache seed --config $TEGOLA_CONFIG_FILE --overwrite --concurrency 3"
 LOCK_FILE=/tmp/openmap_update
 
 if [ -f $LOCK_FILE ]; then
@@ -42,6 +43,11 @@ fi
 # remove outside objects
 psql -d osm -U postgres < remove_outside_objects.sql
 
+echo "Refreshing materialized views " `date`
+psql -d osm -U postgres < update_mv.sql
+echo "Refreshing waterbody labels " `date`
+psql -d osm -U postgres < update_water_labels.sql
+
 # atsiminti dienos kaladėles savaitgaliui (šeštadieniui)
 cat dirty_tiles >> dirty_tiles_weekly
 
@@ -77,43 +83,19 @@ md5sum $DATAFILE > $DATAFILE.md5
 # render expired
 if [ -s dirty_tiles ]; then
     echo "Preparing tile lists " `date`
-    grep -E "^(15|16|17|18)" dirty_tiles > delete_openmap_$DIRTY_FILE
-    grep -E "^(14)" dirty_tiles > generate_openmap_14_$DIRTY_FILE
-    grep -E "^(13)" dirty_tiles > generate_openmap_13_$DIRTY_FILE
-    grep -E "^(12)" dirty_tiles > generate_openmap_12_$DIRTY_FILE
-    grep -E "^(11)" dirty_tiles > generate_openmap_11_$DIRTY_FILE
-    grep -E "^(10)" dirty_tiles > generate_openmap_10_$DIRTY_FILE
-    grep -E "^(9)"  dirty_tiles > generate_openmap_9_$DIRTY_FILE
-    grep -E "^(8)"  dirty_tiles > generate_openmap_8_$DIRTY_FILE
-    grep -E "^(7)"  dirty_tiles > generate_openmap_7_$DIRTY_FILE
 
     grep -E "^(10|11|12|13|14)" dirty_tiles > generate_bicycle_$DIRTY_FILE
     grep -E "^(10|11|12|13|14)" dirty_tiles > generate_craftbeer_$DIRTY_FILE
 
-    echo "Refreshing materialized views " `date`
-    psql -d osm -U postgres < update_mv.sql
-    echo "Refreshing waterbody labels " `date`
-    psql -d osm -U postgres < update_water_labels.sql
-
     echo "OpenMap.lt delete expired tiles large scale tiles in layer 'all' " `date`
-    ../tegola cache purge --config $TEGOLA_CONFIG_FILE --map="all" tile-list delete_openmap_$DIRTY_FILE
+    rm -rf ../cache/all/1[6-8] || true
 
-    echo "OpenMap.lt regenerate expired 14 " `date`
-    ../tegola cache seed --config $TEGOLA_CONFIG_FILE --map="all" tile-list generate_openmap_14_$DIRTY_FILE --overwrite --concurrency 3
-    echo "OpenMap.lt regenerate expired 13 " `date`
-    ../tegola cache seed --config $TEGOLA_CONFIG_FILE --map="all" tile-list generate_openmap_13_$DIRTY_FILE --overwrite --concurrency 3
-    echo "OpenMap.lt regenerate expired 12 " `date`
-    ../tegola cache seed --config $TEGOLA_CONFIG_FILE --map="all" tile-list generate_openmap_12_$DIRTY_FILE --overwrite --concurrency 3
-    echo "OpenMap.lt regenerate expired 11 " `date`
-    ../tegola cache seed --config $TEGOLA_CONFIG_FILE --map="all" tile-list generate_openmap_11_$DIRTY_FILE --overwrite --concurrency 3
-    echo "OpenMap.lt regenerate expired 10 " `date`
-    ../tegola cache seed --config $TEGOLA_CONFIG_FILE --map="all" tile-list generate_openmap_10_$DIRTY_FILE --overwrite --concurrency 3
-    echo "OpenMap.lt regenerate expired 9 " `date`
-    ../tegola cache seed --config $TEGOLA_CONFIG_FILE --map="all" tile-list generate_openmap_9_$DIRTY_FILE --overwrite --concurrency 3
-    echo "OpenMap.lt regenerate expired 8 " `date`
-    ../tegola cache seed --config $TEGOLA_CONFIG_FILE --map="all" tile-list generate_openmap_8_$DIRTY_FILE --overwrite --concurrency 3
-    echo "OpenMap.lt regenerate expired 7 " `date`
-    ../tegola cache seed --config $TEGOLA_CONFIG_FILE --map="all" tile-list generate_openmap_7_$DIRTY_FILE --overwrite --concurrency 3
+    for ZOOM in $(seq 15 -1 7); do
+        echo "OpenMap.lt regenerate expired $ZOOM " `date`
+        grep -E "^$ZOOM/" dirty_tiles > generate_openmap_$ZOOM_$DIRTY_FILE
+        $TEGOLA_SEED --map="all" tile-list generate_openmap_$ZOOM_$DIRTY_FILE > /dev/null
+    done
+    rm generate_openmap_*_$DIRTY_FILE
 
     ../tegola cache purge --config $TEGOLA_CONFIG_FILE --map="detail" tile-list dirty_tiles
     ../tegola cache purge --config $TEGOLA_CONFIG_FILE --map="topo"   tile-list dirty_tiles
@@ -128,8 +110,6 @@ if [ -s dirty_tiles ]; then
 
     echo "Done " `date`
 
-    rm delete_openmap_$DIRTY_FILE
-    rm generate_openmap_*_$DIRTY_FILE
     rm generate_bicycle_$DIRTY_FILE
     rm generate_craftbeer_$DIRTY_FILE
 fi
