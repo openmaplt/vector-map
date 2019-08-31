2548e8e58bdb559070a565edeac49c9e0022398e
tomass <tomasstraupis@gmail.com>
2019-08-31 16:21:20 +0300

    Perjungimas tarp žemėlapių (topo)

---
+++ demo/switch.js
@@ -0,0 +1,139 @@
+var extended = 0;
+var c;
+var e;
+var s_initial = '<img id="sw_orto" src="map_orto.png" onClick="sw_toggle_map()"><br>Orto';
+var s_extended = '<div id="sw1" class="sw_map sw_map1 sw_map_hidden" onClick="sw_switch_to_map(\'-\')"><img src="map_general.png"><br>Bendras</div>' +
+                 '<div id="sw2" class="sw_map sw_map2 sw_map_hidden" onClick="sw_switch_to_map(\'places\')"><img src="map.png"><br>Lankytinos</div>' +
+                 '<div id="sw3" class="sw_map sw_map3 sw_map_hidden" onClick="sw_switch_to_map(\'dviraciai\')"><img src="map_bicycle.png"><br>Dviračiai</div>' +
+                 '<div id="sw4" class="sw_map sw_map4 sw_map_hidden" onClick="sw_switch_to_map(\'topo\')"><img src="map_topo.png"><br>Topografinis</div>' +
+                 '<div id="sw5" class="sw_map sw_map5 sw_map_hidden" onClick="sw_switch_to_map(\'upes\')"><img src="map_upes.png"><br>Upės (baidarės)</div>';
+var isDown = false;
+var startX;
+var scrollLeft;
+var moved = false;
+var orto = false;
+var img_orto = 'map_orto.png';
+var img_map = 'map_upes.png';
+function sw_init(m, toMap, toOrto) {
+  switch (m) {
+    case 1: img_map = 'map_general.png'; break;
+    case 2: img_map = 'map.png'; break;
+    case 3: img_map = 'map_bicycle.png'; break;
+    case 4: img_map = 'map_topo.png'; break;
+    case 5: img_map = 'map_upes.png'; break;
+  }
+  c = document.getElementById('sw_container');
+  c.classList.add('sw_container');
+  c.innerHTML = '<div id="sw_extend" onClick="sw_toggle()"><img id="sw_button" src="expand_button.png"></div><div id="sw_list" class="sw_list"></div>';
+  e = document.getElementById('sw_list');
+  e.innerHTML = s_initial;
+} // init
+
+function sw_toggle() {
+  if (extended == 0) {
+    extended = 1;
+    c.classList.add("sw_container_extended");
+    e.classList.add("sw_list_extended");
+    e.innerHTML = s_extended;
+    setTimeout(function() {
+      document.getElementById('sw1').classList.remove('sw_map_hidden');
+      document.getElementById('sw2').classList.remove('sw_map_hidden');
+      document.getElementById('sw3').classList.remove('sw_map_hidden');
+      document.getElementById('sw4').classList.remove('sw_map_hidden');
+      document.getElementById('sw5').classList.remove('sw_map_hidden');
+      isDown = false;
+      e.addEventListener('mousedown', (ev) => {
+        ev.preventDefault();
+        //console.log('down');
+        isDown = true;
+        moved = false;
+        startX = ev.pageX - e.offsetLeft;
+        scrollLeft = e.scrollLeft;
+      });
+      e.addEventListener('touchstart', (ev) => {
+        //ev.preventDefault();
+        touch = ev.changedTouches[0];
+        //console.log('tdown' + touch.pageX);
+        isDown = true;
+        startX = touch.pageX - e.offsetLeft;
+        scrollLeft = e.scrollLeft;
+      });
+      e.addEventListener('mouseleave', () => {
+        //console.log('leave');
+        isDown = false;
+        moved = true;
+      });
+      e.addEventListener('mouseup', () => {
+        //console.log('up');
+        isDown = false;
+      });
+      e.addEventListener('touchend', () => {
+        //console.log('tup');
+        isDown = false;
+      });
+      e.addEventListener('touchmove', (ev) => {
+        if (!isDown) return;
+        //console.log('move');
+        ev.preventDefault();
+        touch = ev.changedTouches[0];
+        const x = touch.pageX - e.offsetLeft;
+        const walk = (x - startX);
+        e.scrollLeft = scrollLeft - walk;
+      });
+      e.addEventListener('mousemove', (ev) => {
+        if (!isDown) return;
+        ev.preventDefault();
+        //console.log('tmove' + ev.pageX);
+        const x = ev.pageX - e.offsetLeft;
+        const walk = (x - startX);
+        e.scrollLeft = scrollLeft - walk;
+        moved = true;
+      });
+      document.getElementById('sw_button').src = 'contract_button.png';
+    }, 0);
+  } else {
+    extended = 0;
+    c.classList.remove("sw_container_extended");
+    e.classList.remove("sw_list_extended");
+    e.innerHTML = s_initial;
+    document.getElementById('sw_button').src = 'expand_button.png';
+  }
+} // sw_toggle
+function sw_switch_to_map(m) {
+  if (!moved) {
+    console.log('switch to ' + m);
+    var zoom = map.getZoom();
+    var cent = map.getCenter();
+    var website = "https://";
+    var x;
+    if (m != '-') {
+      website = website + m + '.';
+    }
+    if (m == '-') {
+      x = 'm';
+    } else if (m == 'topo') {
+      x = 't';
+    } else if (m == 'upes') {
+      x = '';
+    } else if (m == 'dviraciai') {
+      x = 'b';
+    }
+    if (m == 'places') {
+      website = website + 'openmap.lt/#m=' + Math.trunc(zoom) + '/' + cent.lng + '/' + cent.lat + '//T';
+    } else {
+      website = website + 'openmap.lt/#' + x + '/' + zoom + '/' + cent.lat + '/' + cent.lng + '/0/0';
+    }
+    window.location.href = website;
+  }
+} // sw_switch_to_map
+function sw_toggle_map() {
+  if (orto) {
+    orto = false;
+    document.getElementById('sw_orto').src = img_orto;
+    toMap();
+  } else {
+    orto = true;
+    document.getElementById('sw_orto').src = img_map;
+    toOrto();
+  }
+} // sw_toggle_map
