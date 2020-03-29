42648587de278174154137d6188b63919f876087
tomass <tomasstraupis@gmail.com>
2020-03-29 22:12:23 +0300

    Greičio ribojimų duomenys

---
+++ data/speed/z15.sql
@@ -0,0 +1,27 @@
+SELECT
+  row_number() over() AS gid,
+  st_asbinary(st_linemerge(st_collect(way))) AS geom,
+  maxspeed,
+  "maxspeed:forward" forward,
+  "maxspeed:backward" backward
+FROM
+  planet_osm_line
+WHERE
+  way && !BBOX! AND
+  (
+    highway IN ('motorway', 'motorway_link',
+               'trunk', 'trunk_link',
+               'primary','primary_link',
+               'secondary', 'secondary_link',
+               'tertiary', 'tertiary_link',
+               'unclassified',
+               'residential',
+               'living_street',
+               'service',
+               'track')
+  ) AND
+  (maxspeed is not null or
+   "maxspeed:forward" is not null
+   "maxspeed:backward" is not null
+  )
+GROUP BY maxspeed, "maxspeed:forward", "maxspeed:backward"
