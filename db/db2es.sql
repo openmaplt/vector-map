b428e1cace16f80b586131ad9b2214243fa1aaea
Motiejus Jakštys <desired.mta@gmail.com>
2020-11-02 14:38:01 +0200

    Elasticsearch paieškai
    Pridedame:
    - elasticsearch localdev'ui.
    - naivų indeksą.
    - duomeų importavimą iš `planet_osm_point` ir `planet_osm_polygon`
    - labai paprastą testą, kuris randa Špunką.
    
    Tolesni žingsniai -- formuoti indeksą ir užklausas taip, kad rastų,
    pvz., "Špunk", ir, aišku, pagal adresą. :)

---
+++ db/db2es.sql
@@ -0,0 +1,46 @@
+SELECT
+    json_strip_nulls (row_to_json(t))
+FROM (
+    SELECT
+        osm_id AS id,
+        "addr:city" AS city,
+        "addr:street" AS street,
+        "addr:housenumber" AS housenumber,
+        "addr:unit" AS unit,
+        name AS name,
+        alt_name AS alt_name,
+        official_name AS official_name,
+        description AS description,
+        ST_AsText (ST_Transform (way, 4326)) AS location
+    FROM
+        planet_osm_point
+    WHERE
+        historic IS NOT NULL
+        OR tourism IN ('hotel', 'motel', 'hostel', 'guest_house', 'camp_site', 'caravan_site')
+        OR amenity IN ('restaurant', 'cafe', 'bar')
+        OR tourism IN ('museum', 'attraction', 'viewpoint')
+        OR waterway IN ('river', 'stream', 'canal')
+        OR admin_level IS NOT NULL
+        OR "addr:city" IS NOT NULL
+    UNION
+    SELECT
+        osm_id AS id,
+        "addr:city" AS city,
+        "addr:street" AS street,
+        "addr:housenumber" AS housenumber,
+        "addr:unit" AS unit,
+        name AS name,
+        alt_name AS alt_name,
+        official_name AS official_name,
+        description AS description,
+        ST_AsText (ST_Transform (ST_PointOnSurface (way), 4326)) AS location
+    FROM
+        planet_osm_polygon
+    WHERE
+        historic IS NOT NULL
+        OR tourism IN ('hotel', 'motel', 'hostel', 'guest_house', 'camp_site', 'caravan_site')
+        OR amenity IN ('restaurant', 'cafe', 'bar')
+        OR tourism IN ('museum', 'attraction', 'viewpoint')
+        OR waterway IN ('river', 'stream', 'canal')
+        OR admin_level IS NOT NULL
+        OR "addr:city" IS NOT NULL) AS t
