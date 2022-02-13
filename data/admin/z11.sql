1e077f2f7214697484684b13c6fd5107b67f1f60
tomass <tomasstraupis@gmail.com>
2022-02-13 18:20:38 +0200

    Pridėtos: administracinių ribų kaladėlės

---
+++ data/admin/z11.sql
@@ -0,0 +1,26 @@
+SELECT
+  osm_id AS gid,
+  st_asmvtgeom(st_union(way),!BBOX!) AS geom,
+  admin_level,
+  (
+    CASE
+      WHEN admin_level = '2'
+        THEN 'country'
+      WHEN admin_level = '4'
+        THEN 'region'
+      WHEN admin_level = '5'
+        THEN 'municipality'
+      WHEN admin_level = '6'
+        THEN 'county'
+      WHEN admin_level = '8'
+        THEN 'locality'
+    END
+  ) AS kind
+FROM
+  planet_osm_line
+WHERE
+  way && !BBOX! AND
+  boundary = 'administrative' AND
+  admin_level IN ('2', '4', '5', '6', '8')
+GROUP BY
+  osm_id, admin_level
---
+++ data/admin/z9-p.sql
@@ -0,0 +1,22 @@
+SELECT
+  osm_id AS gid,
+  st_asmvtgeom(way,!BBOX!) AS geom,
+  admin_level,
+  (
+    CASE
+      WHEN admin_level = '2'
+        THEN 'country'
+      WHEN admin_level = '4'
+        THEN 'region'
+      WHEN admin_level = '5'
+        THEN 'municipality'
+      WHEN admin_level = '6'
+        THEN 'county'
+    END
+  ) AS kind
+FROM
+  planet_osm_polygon
+WHERE
+  way && !BBOX! AND
+  boundary = 'administrative' AND
+  admin_level IN ('2', '4', '5', '6')
